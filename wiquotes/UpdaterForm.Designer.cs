fc7b5b0ec5ee3df82310fc75107c828053334d93
patrycze <patryk.pazura@gmail.com>
2017-03-20 14:16:10 +0100

    created list from ftp

--- wiquotes/UpdaterForm.Designer.cs
+++ wiquotes/UpdaterForm.Designer.cs
@@ -28,11 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.url_bar = new System.Windows.Forms.TextBox();
+            this.list_ftp = new System.Windows.Forms.ListBox();
+            this.added_list = new System.Windows.Forms.ListBox();
+            this.add_list_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // url_bar
+            // 
+            this.url_bar.Location = new System.Drawing.Point(12, 45);
+            this.url_bar.Name = "url_bar";
+            this.url_bar.Size = new System.Drawing.Size(246, 20);
+            this.url_bar.TabIndex = 0;
+            // 
+            // list_ftp
+            // 
+            this.list_ftp.FormattingEnabled = true;
+            this.list_ftp.Location = new System.Drawing.Point(12, 110);
+            this.list_ftp.Name = "list_ftp";
+            this.list_ftp.Size = new System.Drawing.Size(120, 95);
+            this.list_ftp.TabIndex = 1;
+            // 
+            // added_list
+            // 
+            this.added_list.FormattingEnabled = true;
+            this.added_list.Location = new System.Drawing.Point(138, 110);
+            this.added_list.Name = "added_list";
+            this.added_list.Size = new System.Drawing.Size(120, 95);
+            this.added_list.TabIndex = 2;
+            // 
+            // add_list_button
+            // 
+            this.add_list_button.Location = new System.Drawing.Point(12, 6);
+            this.add_list_button.Name = "add_list_button";
+            this.add_list_button.Size = new System.Drawing.Size(120, 33);
+            this.add_list_button.TabIndex = 3;
+            this.add_list_button.Text = "button1";
+            this.add_list_button.UseVisualStyleBackColor = true;
+            this.add_list_button.Click += new System.EventHandler(this.add_list_button_Click);
+            // 
+            // UpdaterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.add_list_button);
+            this.Controls.Add(this.added_list);
+            this.Controls.Add(this.list_ftp);
+            this.Controls.Add(this.url_bar);
+            this.Name = "UpdaterForm";
             this.Text = "UpdaterForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox url_bar;
+        private System.Windows.Forms.ListBox list_ftp;
+        private System.Windows.Forms.ListBox added_list;
+        private System.Windows.Forms.Button add_list_button;
     }
 }
--- wiquotes/UpdaterForm.cs
+++ wiquotes/UpdaterForm.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+using System.Net;
+using System.IO;
 
 namespace wiquotes
 {
@@ -16,5 +20,51 @@ namespace wiquotes
         {
             InitializeComponent();
         }
+
+        private void add_list_button_Click(object sender, EventArgs e)
+        {
+
+                // chyba brakuje zabezpieczenia 
+                string url = url_bar.Text;
+                list_ftp.Items.Clear();
+                if (url_bar.Text == String.Empty)
+                {
+                    MessageBox.Show("PODAJ ADRES");
+                }
+                else
+                {
+                    ObjectFromLibrary OBJ = new ObjectFromLibrary();
+                    WebClient webClient = new WebClient();
+                    Stream data = webClient.OpenRead(url);
+
+                    foreach (var temp in OBJ.UnzipFromStream(data, @"c:\temp"))
+                    {
+                        list_ftp.Items.Add(temp);
+
+                    }
+                }
+            }
+        }
     }
-}
+
+    public class ObjectFromLibrary
+    {
+        public List<string> Name;
+        public ObjectFromLibrary() { }
+
+        public List<string> UnzipFromStream(Stream zipStream, string outFolder)
+        {
+            List<string> LIB = new List<string>();
+            ZipInputStream zipInputStream = new ZipInputStream(zipStream);
+            ZipEntry zipEntry = zipInputStream.GetNextEntry();
+            while (zipEntry != null)
+            {
+                //  String entryFileName = zipEntry.Name;
+                LIB.Add(zipEntry.Name);
+                zipEntry = zipInputStream.GetNextEntry();
+        }
+            return LIB;
+        }
+          
+        }
+        
