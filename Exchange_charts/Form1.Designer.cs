446cebd64194472fc3de32db85e73e5bcf2ffc7c
sswitlik <szczepan.switlik@gmail.com>
2017-03-26 18:14:10 +0200

    Many Charts
    Many charts in one window.
    Szczepan Świtlik

---
+++ Exchange_charts/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Exchange_charts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.diagramsListBox = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(138, 12);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(434, 338);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // diagramsListBox
+            // 
+            this.diagramsListBox.BackColor = System.Drawing.SystemColors.Control;
+            this.diagramsListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.diagramsListBox.FormattingEnabled = true;
+            this.diagramsListBox.Items.AddRange(new object[] {
+            "diagram0",
+            "diagram1",
+            "diagram2",
+            "diagram3"});
+            this.diagramsListBox.Location = new System.Drawing.Point(12, 12);
+            this.diagramsListBox.Name = "diagramsListBox";
+            this.diagramsListBox.Size = new System.Drawing.Size(120, 180);
+            this.diagramsListBox.TabIndex = 1;
+            this.diagramsListBox.SelectedIndexChanged += new System.EventHandler(this.diagramsListBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.diagramsListBox);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Exchange Charts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.CheckedListBox diagramsListBox;
+    }
+}
+
---
+++ Exchange_charts/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Globalization;
+
+namespace Exchange_charts
+{
+    public partial class Form1 : Form
+    {
+        private List<DiagramData> charttest;
+        public Form1()
+        {
+            InitializeComponent();
+            charttest = new List<DiagramData>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            charttest.Add(new DiagramData("WIG20,19940414,1000.00,1000.00,1000.00,1000.00,35800.000"));
+            charttest.Add(new DiagramData("WIG20,19940418,1050.50,1050.50,1050.50,1050.50,49975.000"));
+            charttest.Add(new DiagramData("WIG20,19940419,1124.90,1124.90,1124.90,1124.90,69029.500"));
+            charttest.Add(new DiagramData("WIG20,19940421,1304.80,1304.80,1304.80,1304.80,77075.500"));
+            charttest.Add(new DiagramData("WIG20,19940425,1350.10,1350.10,1350.10,1350.10,114219.000"));
+        }
+
+        private void Display()
+        {
+            chart.Series.Clear();
+            chart.ChartAreas.Clear();
+            chart.Legends.Clear();
+            for (int i = 0; i < diagramsListBox.Items.Count; i++)
+            {
+                if (diagramsListBox.GetItemChecked(i))
+                {
+                    var are = "Area" + i.ToString();
+                    var ser = "Serie" + i.ToString();
+                    var leg = "Legend" + i.ToString();
+
+                    chart.ChartAreas.Add(are);
+                    chart.Series.Add(ser);
+                    chart.Legends.Add(leg);
+
+                    chart.Series[ser].ChartArea = are;
+                    chart.Series[ser].Legend = leg;
+                    chart.Legends[leg].DockedToChartArea = are;
+                    chart.Legends[leg].IsDockedInsideChartArea = false;
+
+                    for (int j = 0; j < charttest.Count; j++)
+                    {
+                        chart.Series["Serie" + i.ToString()].Points.AddXY(j, charttest[j].high);
+                    }
+                }
+            }
+        }
+
+        private void diagramsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Display();
+        }
+    }
+    class DiagramData
+    {
+        public string name;
+        public string date;
+        public double open;
+        public double high;
+        public double low;
+        public double close;
+        public double volume;
+
+        //public double moving_average;
+
+        public string all_data;
+
+        public DiagramData(string line)
+        {
+            string[] tab = line.Split(new char[] { ',' });
+
+            name = tab[0];
+            date = tab[1];
+            open = Double.Parse(tab[2], System.Globalization.CultureInfo.InvariantCulture);
+            high = Double.Parse(tab[3], System.Globalization.CultureInfo.InvariantCulture);
+            low = Double.Parse(tab[4], System.Globalization.CultureInfo.InvariantCulture);
+            close = Double.Parse(tab[5], System.Globalization.CultureInfo.InvariantCulture);
+            volume = Double.Parse(tab[6], System.Globalization.CultureInfo.InvariantCulture);
+
+            all_data = line;
+        }
+    }
+}
